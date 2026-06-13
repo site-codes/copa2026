2dabdbce05a0ccc6a2e1fbf8b30018f029780c0b
site-codes <mattheusoficial44@gmail.com>
2026-06-13 00:20:25 -0300

    feat: adicionar rota de teste da API

--- server.js
+++ server.js
@@ -10,8 +10,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 
-// String de conexão SIMPLES
-const MONGODB_URI = 'mongodb://mattheusoficial44_db_user:uiHvOndEbcaqPcZ1@copa2026-shard-00-00.b2jsqdd.mongodb.net:27017,copa2026-shard-00-01.b2jsqdd.mongodb.net:27017,copa2026-shard-00-02.b2jsqdd.mongodb.net:27017/?ssl=true&replicaSet=atlas-wys4gu-shard-0&authSource=admin&retryWrites=true&w=majority';
+const MONGODB_URI = 'mongodb+srv://mattheusoficial44_db_user:uiHvOndEbcaqPcZ1@copa2026.b2jsqdd.mongodb.net/?retryWrites=true&w=majority&appName=copa2026';
 const DB_NAME = 'copa2026';
 
 let db;
